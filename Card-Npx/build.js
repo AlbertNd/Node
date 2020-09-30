321e49b8dc20427c4f41cba88177a2caf5d48114
Albert Ndizeye <albertndizeye@gmail.com>
2020-09-30 14:00:54 +0200

    up

---
+++ Card-Npx/build.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Albert Ndizeye'),
+  work: chalk.white('Junior web developer @becode'),
+  twitter: chalk.blue('https://twitter.com/albert_ndizeye'),
+  github: chalk.blue('https://github.com/AlbertNd/') + chalk.green('AlbertNd'),
+  linkedin: chalk.blue('https://www.linkedin.com/in/albert-ndizeye/') + chalk.green('Albert Ndizeye'),
+  npx: chalk.red('npx') + ' ' + chalk.white('albertnd'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelTwitter: chalk.white.bold('    Twitter:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelCard: chalk.white.bold('       Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.worksdfsdf
+               twittering + newline + // data.labelTwitter + data.twitter
+               githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               carding // data.labelCard + data.npx
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
